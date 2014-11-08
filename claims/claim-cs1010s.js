d422186fd63b221481776b1127a62696df35c429
Minqi Chen <cnchenminqi@gmail.com>
2014-11-08 20:57:59 +0800

    Update old claim script

--- claims/claim-cs1010s.js
+++ claims/claim-cs1010s.js
@@ -112,47 +112,60 @@ function Claim(config) {
   this.remarks = config.duties;
   this.first_day_of_sem = config.first_day_of_sem;
   this.error = false;
+
   var that = this;
+
+  // Ensure claiming for correct module!
+  if ($('h3:contains("Module")').text().substr(8) !== config.module) {
+    alert('Ensure that the module in config matches that of this page.');
+    // Else you will have invisible claims taking up your time.
+    throw new Error('Incorrect module in config.');
+  }
+
   function createActivity(activity_type, week, day, start_time, end_time) {
-    // obj has the properties: 
     var day_upper = day.toUpperCase();
     try {
-      if (ACTIVITY_DICT[activity_type] == undefined || typeof activity_type != "string") {
-        throw "Activity error: " + activity_type + ". Activity type not supported.";
+      if (ACTIVITY_DICT[activity_type] === undefined || typeof activity_type !== 'string') {
+        throw 'Activity error: ' + activity_type + '. Activity type not supported.';
       }
-      if (typeof week != "number" || week <= 0) {
-        throw "Week error: " + week + ". Week value has to be a positive number.";
+      if (typeof week !== 'number' && week !== 'RECESS' || week <= 0) {
+        throw 'Week error: ' + week + '. Week value has to be a positive number or RECESS.';
       }
-      if (DAY_DICT[day_upper] == undefined || typeof day_upper != "string") {
-        throw "Day error: " + day + ". Day value has to be a valid day string.";
+      if (DAY_DICT[day_upper] === undefined || typeof day_upper !== 'string') {
+        throw 'Day error: ' + day + '. Day value has to be a valid day string.';
       }
+
       function checkTime(time) {
         var start_time_hour = time.slice(0,2);
         var start_time_min = time.slice(2);
-        if (typeof time != "string" || 
+        if (typeof time !== 'string' ||
           time.length != 4 ||
           !(parseInt(start_time_hour) >= 0 && parseInt(start_time_hour) <= 23) ||
-          !(start_time_min == "00" || start_time_min == "30")) {
-          throw "Time error: " + time + ". Time has to be string in 24-hr format at half-hour intervals.";
+          !(start_time_min === '00' || start_time_min === '30')) {
+          throw 'Time error: ' + time + '. Time has to be string in 24-hr format at half-hour intervals.';
         }
       }
+
       checkTime(start_time);
       checkTime(end_time);
       var start_time_hour = parseInt(start_time.slice(0,2));
       var end_time_hour = parseInt(end_time.slice(0,2));
+
       if (start_time_hour > end_time_hour || start_time === end_time) {
-        throw "Time error: end_time: " + end_time + " must be after start_time: " + start_time + ".";
+        throw 'Time error: end_time: ' + end_time + ' must be after start_time: ' + start_time + '.';
       } else if (end_time_hour - start_time_hour > 8) {
-        throw "Time error: " + start_time + " - " + end_time + ". Activity cannot be more than 8 hours.";
-      } 
+        throw 'Time error: ' + start_time + ' - ' + end_time + '. Activity cannot be more than 8 hours.';
+      }
     } catch (err) {
       error = true;
       console.log(err);
     }
-    return function() { 
+
+    return function() {
       that.makeClaim(activity_type, week, day, start_time, end_time);
     };
   }
+
   var activities = config.activities_list_fn();
   this.activities_list = [];
   for (var i = 0; i < activities.length; i++) {
@@ -178,7 +191,11 @@ function Claim(config) {
 
 Claim.prototype.makeClaim = function(activity_type, week, day, start_time, end_time) {
   var day_num = DAY_DICT[day];
-  var number_of_days = (week < 7 ? week - 1 : week)*7 + day_num;
+  if (week === 'RECESS') {
+    var number_of_days = 6*7 + day_num;
+  } else {
+    var number_of_days = (week < 7 ? week - 1 : week)*7 + day_num;
+  }
   var activity_date = new Date();
   activity_date.setTime(this.first_day_of_sem.getTime() + (number_of_days * 24 * 60 * 60 * 1000));
   var claim_date_array = activity_date.toDateString().split(' ');
@@ -215,7 +232,7 @@ Claim.prototype.makeClaim = function(activity_type, week, day, start_time, end_t
 Claim.prototype.deleteAllClaims = function() {
   var that = this;
   function deleteClaim(claim_id) {
-    $.post(POST_URL, { 
+    $.post(POST_URL, {
       mod_c: that.module,
       claim_id: claim_id,
       action: 'DELETE',
@@ -226,10 +243,11 @@ Claim.prototype.deleteAllClaims = function() {
       count += 1;
       if (count === $existing_claims.length) {
         alert('All claims deleted! Press OK to continue.');
-        window.location.href = window.location.protocol +'//'+ window.location.host + END_REDIRECT_URL;
+        window.location.href = window.location.protocol + '//' + window.location.host + END_REDIRECT_URL;
       }
     });
   }
+
   var count = 0;
   var $existing_claims = $('#claim-info-div table [name="claim_id"]');
   $existing_claims.each(function() {
@@ -244,4 +262,4 @@ Claim.prototype.makeAllClaims = function() {
 }
 
 var c = new Claim(config);
-c.makeAllClaims();
+// c.makeAllClaims();
