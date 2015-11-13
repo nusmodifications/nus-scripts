65f0f18ae07a9fde20b8bed17967b3c29fbaa9d4
Minqi <cnchenminqi@gmail.com>
2015-11-13 12:44:22 +0800

    Update CS1101S for AY15/16 Sem 1

--- claims/module-claims/claim-cs1101s.js
+++ claims/module-claims/claim-cs1101s.js
@@ -17,6 +17,7 @@
 
 // To delete all claims on the page, run the function c.deleteAllClaims()
 
+// Updated for: AY15/16 Sem 1
 
 // ***********************************************************
 // CONFIGURE THE RELEVANT PROPERTIES IN THE CONFIG OBJECT
@@ -29,47 +30,65 @@ var config = {
   module: 'CS1101S',
   // Format: YYYY/MM/DD
   // Note: Month is from 0-11, Date is from 1-31
-  // This should be the semester's week 1. For AY14/15 Sem 2, it's Monday, Jan 13
-  first_day_of_sem: new Date(2014, 7, 11),
+  // This should be the semester's week 1. For AY15/16 Sem 1, it's Monday, Aug 10
+  first_day_of_sem: new Date(2015, 7, 10),
   // In case you want to customize the duties field for each activity
   // Do not modify the keys
   duties: {
     'Assignment Marking': 'Graded students\' assignments',
-    'Course Material Preparation': 'Prepared course materials',
-    'Tutorial': 'Conducted tutorial'    
+    'Course Material Preparation': 'Prepared assignments',
+    'Tutorial': 'Conducted tutorial',
+    'Consultation with students': 'Consultation',
+    'Midterm Marking': 'Graded midterm test',
+    'Project Evaluation': 'Evaluated programming contest',
+    'System Preparation/setup': 'Setup of online system'
   },
 
   // The following function should return a list of claim objects that you want to make
   activities_list_fn: function () {
     var activities_list = [];
 
-    // 2h DG * 11 weeks = 22 hours
-    // 2h preparation * 10 weeks = 20 hours
-    // 2h grading * 14 weeks = 28 hours
-    //
+    // Assignment marking: 11 - 1 hr x 11 weeks
+    // Consultation with students: 21 - 3 hrs x 7 weeks
+    // Course material preparation: 6
+    // Midterm marking: 3
+    // Project evaluation: 2 (let's say this is the contests)
+    // System preparation/setup: 5
+    // Tutorial: 22 - 2 hrs x 11 weeks
     // TOTAL: 70 hours
     for (var week = 1; week <= 13; week++) {
-      activities_list.push({
-        activity_type: Claim.ASSIGNMENT_MARKING,
-        week: week,
-        day: 'SATURDAY',
-        start_time: '1300',
-        end_time: '1500'
-      });
+      if (week < 12) {
+        activities_list.push({
+          activity_type: Claim.ASSIGNMENT_MARKING,
+          week: week,
+          day: 'SATURDAY',
+          start_time: '1300',
+          end_time: '1400'
+        });
+      }
+      if (week % 2 == 0 || week == 13) {
+        activities_list.push({
+          activity_type: Claim.CONSULTATION,
+          week: week,
+          day: 'SATURDAY',
+          start_time: '1400',
+          end_time: '1700'
+        });
+      }
 
-      if (week === 1 || week === 8) {
-        // there was no tutorial in week 1 and 8 (PH)
+      if (week === 1 || week === 13) {
+        // there was no tutorial in week 1 and 13
       } else {
         activities_list.push({
           activity_type: Claim.TUTORIAL,
           week: week,
-          day: 'TUESDAY',
-          start_time: '1400',
-          end_time: '1600'
+          day: 'MONDAY',
+          start_time: '1600',
+          end_time: '1800'
         });
       }
 
-      if (week <= 10) {
+      if (week <= 3) {
         activities_list.push({
           activity_type: Claim.COURSE_MATERIAL_PREPARATION,
           week: week,
@@ -81,11 +100,25 @@ var config = {
     }
 
     activities_list.push({
-      activity_type: Claim.ASSIGNMENT_MARKING,
-      week: 'RECESS',
-      day: 'SATURDAY',
-      start_time: '1300',
-      end_time: '1500'
+      activity_type: Claim.MIDTERM_MARKING,
+      week: 7,
+      day: 'WEDNESDAY',
+      start_time: '1700',
+      end_time: '2000'
+    });
+    activities_list.push({
+      activity_type: Claim.PROJECT,
+      week: 8,
+      day: 'WEDNESDAY',
+      start_time: '1700',
+      end_time: '1900'
+    });
+    activities_list.push({
+      activity_type: Claim.SYSTEM_SETUP,
+      week: 1,
+      day: 'TUESDAY',
+      start_time: '1600',
+      end_time: '2100'
     });
     return activities_list;
   }
